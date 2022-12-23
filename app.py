a75f4c0a1cb847c1e1e602a412c3f2acd9f2df93
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-12-23 14:49:48 +0330

    use list and funtion in template

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import flask
 
 app = flask.Flask(__name__)
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '2.2.0'},
+        {'name': 'sqlalchemy', 'version': '3.4.0'},
+        {'name': 'pandas', 'version': '1.5.0'},
+    ]
+
 @app.route('/')
 def index():
-    test_packages = [
-        'package1',
-        'package2',
-        'package3',
-    ]
+    test_packages = get_latest_packages
     return flask.render_template('index.html', packages = test_packages)
 
 if __name__ == '__main__':
